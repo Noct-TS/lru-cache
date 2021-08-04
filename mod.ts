767abc24c385d8cb56c05f26bb64c530454009f5
CobyPowers <cpowers619@gmail.com>
2021-08-03 19:37:11 -0500

    fix(deps): use local yallist instead

--- mod.ts
+++ mod.ts
@@ -1,16 +1,16 @@
 // A linked list to keep track of recently-used-ness
-import Yallist from 'https://deno.land/x/yallist@4.0.0-deno/mod.ts';
+import Yallist from 'https://raw.githubusercontent.com/Noct-TS/yallist/master/mod.ts';
 
-const MAX = Symbol('max')
-const LENGTH = Symbol('length')
-const LENGTH_CALCULATOR = Symbol('lengthCalculator')
-const ALLOW_STALE = Symbol('allowStale')
-const MAX_AGE = Symbol('maxAge')
-const DISPOSE = Symbol('dispose')
-const NO_DISPOSE_ON_SET = Symbol('noDisposeOnSet')
-const LRU_LIST = Symbol('lruList')
-const CACHE = Symbol('cache')
-const UPDATE_AGE_ON_GET = Symbol('updateAgeOnGet')
+const MAX = Symbol('max');
+const LENGTH = Symbol('length');
+const LENGTH_CALCULATOR = Symbol('lengthCalculator');
+const ALLOW_STALE = Symbol('allowStale');
+const MAX_AGE = Symbol('maxAge');
+const DISPOSE = Symbol('dispose');
+const NO_DISPOSE_ON_SET = Symbol('noDisposeOnSet');
+const LRU_LIST = Symbol('lruList');
+const CACHE = Symbol('cache');
+const UPDATE_AGE_ON_GET = Symbol('updateAgeOnGet');
 
 const naiveLength = () => 1;
 
@@ -85,7 +85,6 @@ export interface Options<K, V> {
 // cache is a Map (or PseudoMap) that matches the keys to
 // the Yallist.Node object.
 export default class LRUCache<K, V> {
-
   private [LENGTH_CALCULATOR]: (v: V, k: K) => number;
   private [ALLOW_STALE]: any;
   private [MAX_AGE]: number;
@@ -100,70 +99,67 @@ export default class LRUCache<K, V> {
   constructor(options?: Options<K, V>);
   constructor(max: number);
   constructor(options?: number | Options<K, V>) {
-    if (typeof options === 'number')
-      options = { max: options }
+    if (typeof options === 'number') options = { max: options };
 
-    if (!options)
-      options = {}
+    if (!options) options = {};
 
     if (options.max && (typeof options.max !== 'number' || options.max < 0))
-      throw new TypeError('max must be a non-negative number')
+      throw new TypeError('max must be a non-negative number');
     // Kind of weird to have a default max of Infinity, but oh well.
-    const max = this[MAX] = options.max || Infinity
+    const max = (this[MAX] = options.max || Infinity);
 
-    const lc = options.length || naiveLength
-    this[LENGTH_CALCULATOR] = (typeof lc !== 'function') ? naiveLength : lc
-    this[ALLOW_STALE] = options.stale || false
+    const lc = options.length || naiveLength;
+    this[LENGTH_CALCULATOR] = typeof lc !== 'function' ? naiveLength : lc;
+    this[ALLOW_STALE] = options.stale || false;
     if (options.maxAge && typeof options.maxAge !== 'number')
-      throw new TypeError('maxAge must be a number')
-    this[MAX_AGE] = options.maxAge || 0
-    this[DISPOSE] = options.dispose
-    this[NO_DISPOSE_ON_SET] = options.noDisposeOnSet || false
-    this[UPDATE_AGE_ON_GET] = options.updateAgeOnGet || false
-    this.reset()
+      throw new TypeError('maxAge must be a number');
+    this[MAX_AGE] = options.maxAge || 0;
+    this[DISPOSE] = options.dispose;
+    this[NO_DISPOSE_ON_SET] = options.noDisposeOnSet || false;
+    this[UPDATE_AGE_ON_GET] = options.updateAgeOnGet || false;
+    this.reset();
   }
 
   // resize the cache when the max changes.
   set max(mL) {
     if (typeof mL !== 'number' || mL < 0)
-      throw new TypeError('max must be a non-negative number')
+      throw new TypeError('max must be a non-negative number');
 
-    this[MAX] = mL || Infinity
-    trim(this)
+    this[MAX] = mL || Infinity;
+    trim(this);
   }
 
   /**
    * Same as Options.max. Resizes the cache when the `max` changes.
    */
   get max(): number {
-    return this[MAX]
+    return this[MAX];
   }
 
   set allowStale(allowStale: boolean) {
-    this[ALLOW_STALE] = !!allowStale
+    this[ALLOW_STALE] = !!allowStale;
   }
 
   /**
- * Same as Options.allowStale.
- */
+   * Same as Options.allowStale.
+   */
   get allowStale(): boolean {
-    return this[ALLOW_STALE]
+    return this[ALLOW_STALE];
   }
 
   set maxAge(mA) {
     if (typeof mA !== 'number')
-      throw new TypeError('maxAge must be a non-negative number')
+      throw new TypeError('maxAge must be a non-negative number');
 
-    this[MAX_AGE] = mA
-    trim(this)
+    this[MAX_AGE] = mA;
+    trim(this);
   }
-
 
   /**
    * Same as Options.maxAge. Resizes the cache when the `maxAge` changes.
    */
   get maxAge(): number {
-    return this[MAX_AGE]
+    return this[MAX_AGE];
   }
 
   /**
@@ -171,43 +167,51 @@ export default class LRUCache<K, V> {
    * Same as Options.length.
    */
   set lengthCalculator(lC: (value: V, key: K) => number) {
-    if (typeof lC !== 'function')
-      lC = naiveLength
+    if (typeof lC !== 'function') lC = naiveLength;
 
     if (lC !== this[LENGTH_CALCULATOR]) {
-      this[LENGTH_CALCULATOR] = lC
-      this[LENGTH] = 0
+      this[LENGTH_CALCULATOR] = lC;
+      this[LENGTH] = 0;
       this[LRU_LIST].forEach((hit: LEntry) => {
-        hit.length = this[LENGTH_CALCULATOR](hit.value, hit.key)
-        this[LENGTH] += hit.length
-      })
+        hit.length = this[LENGTH_CALCULATOR](hit.value, hit.key);
+        this[LENGTH] += hit.length;
+      });
     }
-    trim(this)
+    trim(this);
   }
-  get lengthCalculator() { return this[LENGTH_CALCULATOR] }
+  get lengthCalculator() {
+    return this[LENGTH_CALCULATOR];
+  }
 
   /**
    * Return total length of objects in cache taking into account `length` options function.
    */
-  get length() { return this[LENGTH] }
+  get length() {
+    return this[LENGTH];
+  }
 
   /**
-     * Return total quantity of objects currently in cache. Note,
-     * that `stale` (see options) items are returned as part of this item count.
-     */
-  get itemCount() { return this[LRU_LIST].length }
+   * Return total quantity of objects currently in cache. Note,
+   * that `stale` (see options) items are returned as part of this item count.
+   */
+  get itemCount() {
+    return this[LRU_LIST].length;
+  }
 
   /**
    * The same as `cache.forEach(...)` but items are iterated over in reverse order.
    * (ie, less recently used items are iterated over first.)
    */
-  rforEach<T = this>(callbackFn: (this: T, value: V, key: K, cache: this) => void, thisArg?: T): void;
+  rforEach<T = this>(
+    callbackFn: (this: T, value: V, key: K, cache: this) => void,
+    thisArg?: T
+  ): void;
   rforEach(fn: any, thisp: any) {
-    thisp = thisp || this
-    for (let walker = this[LRU_LIST].tail; walker !== null;) {
-      const prev = walker.prev
-      forEachStep(this, fn, walker, thisp)
-      walker = prev
+    thisp = thisp || this;
+    for (let walker = this[LRU_LIST].tail; walker !== null; ) {
+      const prev = walker.prev;
+      forEachStep(this, fn, walker, thisp);
+      walker = prev;
     }
   }
 
@@ -215,13 +219,16 @@ export default class LRUCache<K, V> {
    * Just like `Array.prototype.forEach`. Iterates over all the keys in the cache,
    * in order of recent-ness. (Ie, more recently used items are iterated over first.)
    */
-  forEach<T = this>(callbackFn: (this: T, value: V, key: K, cache: this) => void, thisArg?: T): void;
+  forEach<T = this>(
+    callbackFn: (this: T, value: V, key: K, cache: this) => void,
+    thisArg?: T
+  ): void;
   forEach(fn: any, thisp: any) {
-    thisp = thisp || this
-    for (let walker = this[LRU_LIST].head; walker !== null;) {
-      const next = walker.next
-      forEachStep(this, fn, walker, thisp)
-      walker = next
+    thisp = thisp || this;
+    for (let walker = this[LRU_LIST].head; walker !== null; ) {
+      const next = walker.next;
+      forEachStep(this, fn, walker, thisp);
+      walker = next;
     }
   }
 
@@ -229,29 +236,29 @@ export default class LRUCache<K, V> {
    * Return an array of the keys in the cache.
    */
   keys(): K[] {
-    return this[LRU_LIST].toArray().map((k: LEntry) => k.key)
+    return this[LRU_LIST].toArray().map((k: LEntry) => k.key);
   }
 
   /**
    * Return an array of the values in the cache.
    */
   values(): V[] {
-    return this[LRU_LIST].toArray().map((k: LEntry) => k.value)
+    return this[LRU_LIST].toArray().map((k: LEntry) => k.value);
   }
 
   /**
-     * Clear the cache entirely, throwing away all values.
-     */
+   * Clear the cache entirely, throwing away all values.
+   */
   reset(): void {
-    if (this[DISPOSE] &&
-      this[LRU_LIST] &&
-      this[LRU_LIST].length) {
-      this[LRU_LIST].forEach((hit: LEntry) => this[DISPOSE](hit.key, hit.value))
+    if (this[DISPOSE] && this[LRU_LIST] && this[LRU_LIST].length) {
+      this[LRU_LIST].forEach((hit: LEntry) =>
+        this[DISPOSE](hit.key, hit.value)
+      );
     }
 
-    this[CACHE] = new Map() // hash of items by key
-    this[LRU_LIST] = new Yallist() // list of items in order of use recency
-    this[LENGTH] = 0 // length of items in the list
+    this[CACHE] = new Map(); // hash of items by key
+    this[LRU_LIST] = new Yallist(); // list of items in order of use recency
+    this[LENGTH] = 0; // length of items in the list
   }
 
   /**
@@ -259,15 +266,20 @@ export default class LRUCache<K, V> {
    */
   dump(): Array<Entry<K, V>> {
     return this[LRU_LIST].map((hit: LEntry) =>
-      isStale(this, hit) ? false : {
-        k: hit.key,
-        v: hit.value,
-        e: hit.now + (hit.maxAge || 0)
-      }).toArray().filter((h: any) => h) as any
+      isStale(this, hit)
+        ? false
+        : {
+            k: hit.key,
+            v: hit.value,
+            e: hit.now + (hit.maxAge || 0),
+          }
+    )
+      .toArray()
+      .filter((h: any) => h) as any;
   }
 
   dumpLru() {
-    return this[LRU_LIST]
+    return this[LRU_LIST];
   }
 
   /**
@@ -275,104 +287,100 @@ export default class LRUCache<K, V> {
    * `maxAge` is optional and overrides the cache `maxAge` option if provided.
    */
   set(key: K, value: V, maxAge?: number): boolean {
-    maxAge = maxAge || this[MAX_AGE]
+    maxAge = maxAge || this[MAX_AGE];
 
     if (maxAge && typeof maxAge !== 'number')
-      throw new TypeError('maxAge must be a number')
+      throw new TypeError('maxAge must be a number');
 
-    const now = maxAge ? Date.now() : 0
-    const len = this[LENGTH_CALCULATOR](value, key)
+    const now = maxAge ? Date.now() : 0;
+    const len = this[LENGTH_CALCULATOR](value, key);
 
     if (this[CACHE].has(key)) {
       if (len > this[MAX]) {
-        del(this, this[CACHE].get(key))
-        return false
+        del(this, this[CACHE].get(key));
+        return false;
       }
 
-      const node = this[CACHE].get(key)
-      const item = node.value
+      const node = this[CACHE].get(key);
+      const item = node.value;
 
       // dispose of the old one before overwriting
       // split out into 2 ifs for better coverage tracking
       if (this[DISPOSE]) {
-        if (!this[NO_DISPOSE_ON_SET])
-          this[DISPOSE](key, item.value)
+        if (!this[NO_DISPOSE_ON_SET]) this[DISPOSE](key, item.value);
       }
 
-      item.now = now
-      item.maxAge = maxAge
-      item.value = value
-      this[LENGTH] += len - item.length
-      item.length = len
-      this.get(key)
-      trim(this)
-      return true
+      item.now = now;
+      item.maxAge = maxAge;
+      item.value = value;
+      this[LENGTH] += len - item.length;
+      item.length = len;
+      this.get(key);
+      trim(this);
+      return true;
     }
 
-    const hit = new LEntry(key, value, len, now, maxAge)
+    const hit = new LEntry(key, value, len, now, maxAge);
 
     // oversized objects fall out of cache automatically.
     if (hit.length > this[MAX]) {
-      if (this[DISPOSE])
-        this[DISPOSE](key, value)
+      if (this[DISPOSE]) this[DISPOSE](key, value);
 
-      return false
+      return false;
     }
 
-    this[LENGTH] += hit.length
-    this[LRU_LIST].unshift(hit)
-    this[CACHE].set(key, this[LRU_LIST].head)
-    trim(this)
-    return true
+    this[LENGTH] += hit.length;
+    this[LRU_LIST].unshift(hit);
+    this[CACHE].set(key, this[LRU_LIST].head);
+    trim(this);
+    return true;
   }
 
   /**
- * Check if a key is in the cache, without updating the recent-ness
- * or deleting it for being stale.
- */
+   * Check if a key is in the cache, without updating the recent-ness
+   * or deleting it for being stale.
+   */
   has(key: K): boolean {
-    if (!this[CACHE].has(key)) return false
-    const hit = this[CACHE].get(key).value
-    return !isStale(this, hit)
+    if (!this[CACHE].has(key)) return false;
+    const hit = this[CACHE].get(key).value;
+    return !isStale(this, hit);
   }
 
   /**
- * Will update the "recently used"-ness of the key. They do what you think.
- * `maxAge` is optional and overrides the cache `maxAge` option if provided.
- *
- * If the key is not found, will return `undefined`.
- */
+   * Will update the "recently used"-ness of the key. They do what you think.
+   * `maxAge` is optional and overrides the cache `maxAge` option if provided.
+   *
+   * If the key is not found, will return `undefined`.
+   */
   get(key: K): V | undefined {
-    return get(this, key, true)
+    return get(this, key, true);
   }
 
   /**
- * Returns the key value (or `undefined` if not found) without updating
- * the "recently used"-ness of the key.
- *
- * (If you find yourself using this a lot, you might be using the wrong
- * sort of data structure, but there are some use cases where it's handy.)
- */
+   * Returns the key value (or `undefined` if not found) without updating
+   * the "recently used"-ness of the key.
+   *
+   * (If you find yourself using this a lot, you might be using the wrong
+   * sort of data structure, but there are some use cases where it's handy.)
+   */
   peek(key: K): V | undefined {
-    return get(this, key, false)
+    return get(this, key, false);
   }
 
   pop() {
-    const node = this[LRU_LIST].tail
-    if (!node)
-      return null
+    const node = this[LRU_LIST].tail;
+    if (!node) return null;
 
-    del(this, node)
-    return node.value
+    del(this, node);
+    return node.value;
   }
 
   /**
    * Deletes a key out of the cache.
    */
   del(key: K): void {
-    del(this, this[CACHE].get(key))
+    del(this, this[CACHE].get(key));
   }
-
 
   /**
    * Loads another cache entries array, obtained with `sourceCache.dump()`,
@@ -382,108 +390,106 @@ export default class LRUCache<K, V> {
    */
   load(arr: ReadonlyArray<Entry<K, V>>): void {
     // reset the cache
-    this.reset()
+    this.reset();
 
-    const now = Date.now()
+    const now = Date.now();
     // A previous serialized cache has the most recent items first
     for (let l = arr.length - 1; l >= 0; l--) {
-      const hit = arr[l]
-      const expiresAt = hit.e || 0
+      const hit = arr[l];
+      const expiresAt = hit.e || 0;
       if (expiresAt === 0)
         // the item was created without expiration in a non aged cache
-        this.set(hit.k, hit.v)
+        this.set(hit.k, hit.v);
       else {
-        const maxAge = expiresAt - now
+        const maxAge = expiresAt - now;
         // dont add already expired items
         if (maxAge > 0) {
-          this.set(hit.k, hit.v, maxAge)
+          this.set(hit.k, hit.v, maxAge);
         }
       }
     }
   }
 
   /**
-     * Manually iterates over the entire cache proactively pruning old entries.
-     */
+   * Manually iterates over the entire cache proactively pruning old entries.
+   */
   prune(): void {
-    this[CACHE].forEach((value: any, key: any) => get(this, key, false))
+    this[CACHE].forEach((value: any, key: any) => get(this, key, false));
   }
 }
 
 const get = (self: any, key: any, doUse: any) => {
-  const node = self[CACHE].get(key)
+  const node = self[CACHE].get(key);
   if (node) {
-    const hit = node.value
+    const hit = node.value;
     if (isStale(self, hit)) {
-      del(self, node)
-      if (!self[ALLOW_STALE])
-        return undefined
+      del(self, node);
+      if (!self[ALLOW_STALE]) return undefined;
     } else {
       if (doUse) {
-        if (self[UPDATE_AGE_ON_GET])
-          node.value.now = Date.now()
-        self[LRU_LIST].unshiftNode(node)
+        if (self[UPDATE_AGE_ON_GET]) node.value.now = Date.now();
+        self[LRU_LIST].unshiftNode(node);
       }
     }
-    return hit.value
+    return hit.value;
   }
-}
+};
 
 const isStale = (self: any, hit: any) => {
-  if (!hit || (!hit.maxAge && !self[MAX_AGE]))
-    return false
+  if (!hit || (!hit.maxAge && !self[MAX_AGE])) return false;
 
-  const diff = Date.now() - hit.now
-  return hit.maxAge ? diff > hit.maxAge
-    : self[MAX_AGE] && (diff > self[MAX_AGE])
-}
+  const diff = Date.now() - hit.now;
+  return hit.maxAge ? diff > hit.maxAge : self[MAX_AGE] && diff > self[MAX_AGE];
+};
 
 const trim = (self: any) => {
   if (self[LENGTH] > self[MAX]) {
-    for (let walker = self[LRU_LIST].tail;
-      self[LENGTH] > self[MAX] && walker !== null;) {
+    for (
+      let walker = self[LRU_LIST].tail;
+      self[LENGTH] > self[MAX] && walker !== null;
+
+    ) {
       // We know that we're about to delete this one, and also
       // what the next least recently used key will be, so just
       // go ahead and set it now.
-      const prev = walker.prev
-      del(self, walker)
-      walker = prev
+      const prev = walker.prev;
+      del(self, walker);
+      walker = prev;
     }
   }
-}
+};
 
 const del = (self: any, node: any) => {
   if (node) {
-    const hit = node.value
-    if (self[DISPOSE])
-      self[DISPOSE](hit.key, hit.value)
+    const hit = node.value;
+    if (self[DISPOSE]) self[DISPOSE](hit.key, hit.value);
 
-    self[LENGTH] -= hit.length
-    self[CACHE].delete(hit.key)
-    self[LRU_LIST].removeNode(node)
+    self[LENGTH] -= hit.length;
+    self[CACHE].delete(hit.key);
+    self[LRU_LIST].removeNode(node);
   }
-}
+};
 
 class LEntry {
-  constructor(public key: any
-    , public value: any
-    , public length: any
-    , public now: any
-    , public maxAge: any) {
-    this.maxAge = maxAge || 0
+  constructor(
+    public key: any,
+    public value: any,
+    public length: any,
+    public now: any,
+    public maxAge: any
+  ) {
+    this.maxAge = maxAge || 0;
   }
 }
 
 const forEachStep = (self: any, fn: any, node: any, thisp: any) => {
-  let hit = node.value
+  let hit = node.value;
   if (isStale(self, hit)) {
-    del(self, node)
-    if (!self[ALLOW_STALE])
-      hit = undefined
+    del(self, node);
+    if (!self[ALLOW_STALE]) hit = undefined;
   }
-  if (hit)
-    fn.call(thisp, hit.value, hit.key, self)
-}
+  if (hit) fn.call(thisp, hit.value, hit.key, self);
+};
 
 export interface Entry<K, V> {
   k: K;
